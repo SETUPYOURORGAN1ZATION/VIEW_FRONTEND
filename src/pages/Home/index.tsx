e782af51ecb79802d69b9194a18d94d07ce0b9be
jyh071116 <jyh071116@gmail.com>
2023-12-27 22:24:42 +0900

    feat: Home 페이지 퍼블리싱

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
 import Grass from "components/Grass";
 import DayInfo from "interfaces/DayInfo";
 import banner from "assets/banner.png";
+import calendar from "assets/calendar.svg";
 import * as S from "./style";
 
 const Home = () => {
   const data: DayInfo[] = [
     { date: "2023-09-02", status: "TRUANCY", description: null },
     { date: "2023-09-03", status: "ATTENDANCE", description: null },
+    { date: "2023-09-04", status: "ATTENDANCE", description: null },
+    { date: "2023-09-05", status: "ATTENDANCE", description: null },
     { date: "2023-10-06", status: "ABSENT", description: "배고파서" },
     { date: "2023-12-27", status: "ATTENDANCE", description: null },
   ];
@@ -14,8 +17,37 @@ const Home = () => {
   return (
     <S.Layout>
       <S.Banner src={banner} />
+      <S.StudentId>2학년 2반 10번</S.StudentId>
+      <S.Name>박우빈님</S.Name>
       <S.Text>출결 현황</S.Text>
       <Grass data={data} attendColor="#0085ff" />
+      <S.Box>
+        <S.PassTicketBox>
+          <S.PassTicketMainText>이석증 발급하기</S.PassTicketMainText>
+          <S.ContentBox>
+            <S.ReasonBox>
+              <S.PassTicketText>✉️ 사유</S.PassTicketText>
+              <S.ReasonArea placeholder="사유를 입력하세요." />
+            </S.ReasonBox>
+            <S.DateAndSubmitBox>
+              <S.PassTicketText>🗓️ 날짜</S.PassTicketText>
+              <S.DateBox>
+                <S.DateInput type="date" />
+              </S.DateBox>
+              <S.SubmitButton>신청하기</S.SubmitButton>
+            </S.DateAndSubmitBox>
+          </S.ContentBox>
+        </S.PassTicketBox>
+        <S.TodayAttendBox>
+          <S.HeaderBox>
+            <S.HeaderText>2023년 12월 27일</S.HeaderText>
+            <S.TodayStatus />
+          </S.HeaderBox>
+          <S.Empty />
+          <S.TodayStatusText>출석 완료</S.TodayStatusText>
+          <S.StudentIdAndName>2210 박우빈</S.StudentIdAndName>
+        </S.TodayAttendBox>
+      </S.Box>
     </S.Layout>
   );
 };
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -10,11 +10,161 @@ export const Layout = styled.div`
 `;
 
 export const Banner = styled.img`
+  position: relative;
   width: 100%;
   margin-bottom: 30px;
+`;
+
+export const StudentId = styled.div`
+  position: absolute;
+  top: 20%;
+  left: 15%;
+  color: #cccccc;
+  ${font.H2}
+  font-weight: 500;
+`;
+
+export const Name = styled.div`
+  position: absolute;
+  top: 25%;
+  left: 15%;
+  color: white;
+  ${font.D2}
 `;
 
 export const Text = styled.div`
   ${font.H3}
   margin-bottom: 20px;
+`;
+
+export const Box = styled.div`
+  margin-top: 50px;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const PassTicketBox = styled.div`
+  padding: 2.5% 3%;
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  aspect-ratio: 8/3;
+  box-shadow: -1px 0px 4px rgba(0, 0, 0, 0.08);
+`;
+
+export const PassTicketMainText = styled.div`
+  ${font.H2}
+  font-weight: 500;
+`;
+
+export const ContentBox = styled.div`
+  margin-top: 10px;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  gap: 10%;
+`;
+
+export const ReasonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 65%;
+  height: 100%;
+`;
+
+export const PassTicketText = styled.div`
+  font-size: 1.2rem;
+  margin-bottom: 2%;
+`;
+
+export const ReasonArea = styled.textarea`
+  margin-top: 10px;
+  width: 100%;
+  height: 100%;
+  border: none;
+  resize: none;
+  padding: 2.5% 3.5%;
+  font-size: 1.2rem;
+  border-radius: 5px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.05);
+`;
+
+export const DateAndSubmitBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 45px;
+  width: 35%;
+  height: 30%;
+`;
+
+export const DateBox = styled.div`
+  margin-top: -30px;
+  display: flex;
+  gap: 20px;
+  font-size: 1.2rem;
+  padding: 5% 5%;
+  align-items: center;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.05);
+`;
+
+export const DateInput = styled.input`
+  border: none;
+  font-size: 1.2rem;
+  background-color: transparent;
+`;
+
+export const SubmitButton = styled.div`
+  display: flex;
+  border-radius: 5px;
+  padding: 8% 0;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: rgba(0, 133, 255, 0.5);
+`;
+
+export const TodayAttendBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 45%;
+  padding: 2.5% 3%;
+  box-shadow: -1px 0px 4px rgba(0, 0, 0, 0.08);
+  margin-right: 2px;
+`;
+
+export const HeaderBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 20%;
+`;
+
+export const Empty = styled.div`
+  height: 40%;
+`;
+
+export const HeaderText = styled.div`
+  ${font.H2}
+  font-weight: 500;
+`;
+
+export const TodayStatus = styled.div`
+  height: 65%;
+  aspect-ratio: 1;
+  background-color: #0085ff;
+  border-radius: 50px;
+`;
+
+export const TodayStatusText = styled.div`
+  ${font.D3}
+  font-size: 2.5rem;
+  font-weight: 600;
+`;
+
+export const StudentIdAndName = styled.div`
+  margin-top: 10px;
+  ${font.H3}
+  color: #999999;
+  font-weight: 500;
 `;
