15d34dbedf79ca46d422ec03b054c25f6f1ff998
Ubinquitous <ubinquitous1@gmail.com>
2023-12-27 17:32:59 +0900

    a

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -4,7 +4,7 @@ const fontGenerator = (
   lineHeight: number,
   letterSpacing: number
 ) => `
-      font-family: 'Pretendard', sans-serif;
+      font-family: 'Pretendard Variable', sans-serif;
       font-weight: ${weight};
       font-size: ${size}rem;
       line-height: ${lineHeight}%;
