888243896b631e3c4c0b43bcaa696ab9825753c5
jyh071116 <jyh071116@gmail.com>
2023-12-27 17:21:53 +0900

    chore(config): 디렉터리 설계

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  PassTicket,
+  Register,
+  StudentRegister,
+  Login,
+  SignUp,
+} from "pages";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register/:studentId" element={<StudentRegister />} />
+        <Route path="/passticket" element={<PassTicket />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/siginup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as PassTicket } from "./PassTicket";
+export { default as Register } from "./Register";
+export { default as StudentRegister } from "./StudentRegister";
+export { default as Login } from "./Login";
+export { default as SignUp } from "./SignUp";
