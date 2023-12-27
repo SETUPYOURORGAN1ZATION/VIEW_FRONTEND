21f2a67f70cb1ad4a2d8b9979a79802393a90ab1
Ubinquitous <ubinquitous1@gmail.com>
2023-12-27 19:18:23 +0900

    assv

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
+import DropDown from "components/DropDown";
+import Retry from "components/Retry";
+
+const mocks = [
+  {
+    grade: 2,
+    classNum: 1,
+    list: Array.from({ length: 16 }).map(() => ({
+      status: "ABSENT",
+      studentNumber: "2210",
+      studentName: "박우빈",
+      description: "",
+    })),
+  },
+  {
+    grade: 2,
+    classNum: 2,
+    list: Array.from({ length: 16 }).map(() => ({
+      status: "ATTENDANCE",
+      studentNumber: "2210",
+      studentName: "박우빈",
+      description: "",
+    })),
+  },
+  {
+    grade: 2,
+    classNum: 3,
+    list: Array.from({ length: 16 }).map(() => ({
+      status: "TRUANCY",
+      studentNumber: "2210",
+      studentName: "박우빈",
+      description: "UNITHON 참석으로 수~금 3일 결석",
+    })),
+  },
+  {
+    grade: 2,
+    classNum: 4,
+    list: Array.from({ length: 16 }).map(() => ({
+      status: "abtend",
+      studentNumber: "2210",
+      studentName: "박우빈",
+      description: "",
+    })),
+  },
+];
 
 const Register = () => {
+  const [currGrade, setCurrGrade] = useState("1학년");
+  const [currClass, setCurrClass] = useState("1반");
+  const isCurrRegister = (grade: number, classNum: number) =>
+    grade === +currGrade[0] && classNum === +currClass[0];
+  const filteredDataList = mocks.filter(({ grade, classNum }) =>
+    isCurrRegister(grade, classNum)
+  );
+
   return (
     <S.Container>
       출결현황
       <S.PageTitle>🗓️ 2023년 12월 27일</S.PageTitle>
+      <S.List>
+        <Retry />
+        <DropDown
+          state={currGrade}
+          setState={setCurrGrade}
+          list={["1학년", "2학년", "3학년"]}
+        />
+        <DropDown
+          state={currClass}
+          setState={setCurrClass}
+          list={["1반", "2반", "3반", "4반"]}
+        />
+      </S.List>
+      {filteredDataList.map((item) => (
+        <S.RegisterContainer>
+          <S.RegisterListTitle>
+            {item.grade}학년 {item.classNum}반
+          </S.RegisterListTitle>
+          <S.RegisterList>
+            {item.list.map((register) => (
+              <S.RegisterListItem status={register.status}>
+                <S.RegisterListItemHead>
+                  <S.StatusCircle status={register.status} />
+                  <S.StatusTitle>
+                    {(() => {
+                      switch (register.status) {
+                        case "ATTENDANCE":
+                          return "출석 완료";
+                        case "ABSENT":
+                          return "미인정 결석";
+                        case "TRUANCY":
+                          return "인정 결석";
+                        default:
+                          return "미확인";
+                      }
+                    })()}
+                  </S.StatusTitle>
+                  <S.StatusUpdateButton>상태 변경</S.StatusUpdateButton>
+                </S.RegisterListItemHead>
+                <S.RegisterListItemContent>
+                  {register.studentNumber} {register.studentName}
+                </S.RegisterListItemContent>
+                <S.RegisterListItemDescription>
+                  {register.description || "-"}
+                </S.RegisterListItemDescription>
+              </S.RegisterListItem>
+            ))}
+          </S.RegisterList>
+        </S.RegisterContainer>
+      ))}
     </S.Container>
   );
 };
--- src/pages/Register/style.ts
+++ src/pages/Register/style.ts
@@ -6,8 +6,107 @@ export const Container = styled.div`
   min-height: 100vh;
   /* */
   padding-top: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 export const PageTitle = styled.h1`
   ${font.H2};
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
+
+export const RegisterContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const RegisterList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const RegisterListTitle = styled.h1`
+  ${font.H3};
+`;
+
+export const RegisterListItem = styled.li<{ status: string }>`
+  width: 24%;
+  height: fit-content;
+  margin: 16px 0;
+  padding: 10px 18px;
+  display: flex;
+  gap: 4px;
+  flex-direction: column;
+  border-right: 4px solid
+    ${({ status }) => {
+      switch (status) {
+        case "ATTENDANCE":
+          return "#0085FF";
+        case "ABSENT":
+          return "#E54F5A";
+        case "TRUANCY":
+          return "#FFE500";
+        default:
+          return "#aaa";
+      }
+    }};
+  box-shadow: -1px 0px 4px 0px rgba(0, 0, 0, 0.08);
+`;
+
+export const RegisterListItemHead = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const StatusCircle = styled.div<{ status: string }>`
+  width: 18px;
+  height: 18px;
+  border-radius: 9999px;
+  background-color: ${({ status }) => {
+    switch (status) {
+      case "ATTENDANCE":
+        return "#0085FF";
+      case "ABSENT":
+        return "#E54F5A";
+      case "TRUANCY":
+        return "#FFE500";
+      default:
+        return "#aaa";
+    }
+  }};
+`;
+
+export const StatusTitle = styled.span`
+  ${font.p2};
+  color: #999;
+`;
+
+export const StatusUpdateButton = styled.span`
+  ${font.context};
+  font-size: 12px;
+  color: #999;
+  margin-left: auto;
+`;
+
+export const RegisterListItemContent = styled.span`
+  ${font.H5};
+  font-weight: 500;
+`;
+
+export const RegisterListItemDescription = styled.span`
+  ${font.p3};
+  color: #aaa;
 `;
